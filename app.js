38a1b4f5b617d50fb237dfbbc904d10986e130f8
Sangbong Park <sangbong.park@outlook.com>
2021-05-11 12:38:58 -0400

    Init Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('HELLO');
+})
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`);
+})
